ad36dd8ec18e2ba71bae3c27057c4996e1ebf0b8
sharif474 <sharif74a@gmail.com>
2023-09-17 01:11:02 +0600

    add main funtion

---
+++ 6.2.cpp
@@ -0,0 +1,12 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+for(a=100;a>0;a--){
+    cout<<a<<endl;
+}
+    return 0;
+}
