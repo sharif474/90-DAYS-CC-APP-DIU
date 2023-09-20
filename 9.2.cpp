d780b70ffe2ce7df1070fe366f40d105f9d27577
sharif474 <sharif74a@gmail.com>
2023-09-20 23:57:14 +0600

    add main funtion

---
+++ 9.2.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+ int a = 8, b = 3;
+ 
+    // Addition operator
+    cout << "a + b = " << (a + b) << endl;
+   
+    // Subtraction operator
+    cout << "a - b = " << (a - b) << endl;
+   
+    // Multiplication operator
+    cout << "a * b = " << (a * b) << endl;
+   
+    // Division operator
+    cout << "a / b = " << (a / b) << endl;
+   
+    // Modulo operator
+    cout << "a % b = " << (a % b) << endl;
+
+    return 0;
+}
