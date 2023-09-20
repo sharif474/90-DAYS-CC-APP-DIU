27143aeb75f75d38967c9b5d56c3b29499063645
sharif474 <sharif74a@gmail.com>
2023-09-20 23:45:08 +0600

    add main funtion

---
+++ 8.3.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+ string a ;
+ cout << "give a input :" << endl
+ ;
+ cin >> a;
+ cout << a;
+
+    return 0;
+}
---
+++ 9.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+ string x,y ;
+ cout << "Enter the value of x :" ;
+ cin >> x;
+ cout << "Enter the value of y  :" ;
+ cin >> y;
+ if (x>y){
+    cout << "X is biggre " ;
+ }
+ else{cout << "y is bigger";}
+
+    return 0;
+}
