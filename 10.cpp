fe1073a9e1ce3370a1191260125b60480bdc73a3
sharif474 <sharif74a@gmail.com>
2023-09-21 23:28:52 +0600

    add main funtion

---
+++ 10.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+ int time = 22;
+if (time < 10) {
+  cout << "Good morning.";
+} else if (time < 20) {
+  cout << "Good day.";
+} else {
+  cout << "Good evening.";
+}
+
+    return 0;
+}
