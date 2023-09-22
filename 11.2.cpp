8244b3cb31b9c4829b331082cba26c5bd6cdeeec
sharif474 <sharif74a@gmail.com>
2023-09-22 23:46:33 +0600

    add to git

---
+++ 11.2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+ for (int i = 1; i <= 2; ++i) {
+  cout << "Outer: " << i << "\n"; // Executes 2 times
+
+  // Inner loop
+  for (int j = 1; j <= 3; ++j) {
+    cout << " Inner: " << j << "\n"; // Executes 6 times (2 * 3)
+  }
+}
+
+    return 0;
+}
