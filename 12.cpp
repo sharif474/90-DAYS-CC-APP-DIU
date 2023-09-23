9111400ffbbb98707b9f00b6fa28adcaa6d9132e
sharif474 <sharif74a@gmail.com>
2023-09-23 23:10:52 +0600

    add to main function

---
+++ 12.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+ for (int i = 0; i < 10; i++) {
+  if (i == 4) {
+    break;
+  }
+  cout << i << "\n";
+}
+    return 0;
+}
