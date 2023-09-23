7adb53ecfad6b89934b839af59e74a1542b7f183
sharif474 <sharif74a@gmail.com>
2023-09-23 23:40:23 +0600

    add to main function

---
+++ 12.2.cpp
@@ -0,0 +1,15 @@
+
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+string cars[5] = {"Volvo", "BMW", "Ford", "Mazda", "Tesla"};
+for (int i = 0; i < 5; i++) {
+  cout << cars[i] << "\n";
+}
+      return 0;
+}
