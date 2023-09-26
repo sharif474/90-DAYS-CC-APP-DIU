cd4c97a78b889348be526f4dc7e2394cdeaa47d6
sharif474 <sharif74a@gmail.com>
2023-09-26 11:20:31 +0600

    add to main function

---
+++ 16.3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void myFunction(string country = "Norway") {
+  cout << country << "\n";
+}
+
+int main() {
+  myFunction("Sweden");
+  myFunction("India");
+  myFunction();
+  myFunction("USA");
+  return 0;
+}
+
