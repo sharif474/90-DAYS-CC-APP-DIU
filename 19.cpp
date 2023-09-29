5608d5134eb3875a3e24e37ea4f087fe4e7ba6f4
sharif474 <sharif74a@gmail.com>
2023-09-30 00:23:46 +0600

    add

---
+++ 19.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int sum(int k) {
+  if (k > 0) {
+    return k + sum(k - 2);
+  } else {
+    return 0;
+  }
+}
+
+int main() {
+  int result = sum(1000);
+  cout << result;
+  return 0;
+}
