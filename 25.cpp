0c7e59dd21a570fcac13ecc14fa032223a2c2785
sharif474 <sharif74a@gmail.com>
2023-10-05 02:53:29 +0600

    add to main function

---
+++ 25.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y,a;
+	    cin>>x>>y>>a;
+	    if((a>=y) && (a<x)){
+	        cout<<"yes"<<endl;
+	    }
+	    else{
+	        cout<<"no"<<endl;
+	    }
+	    
+	}
+	return 0;
+}
+
