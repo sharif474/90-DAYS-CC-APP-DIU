59aee0a9362154171109dedeafcba0f4dbb5472b
sharif474 <sharif74a@gmail.com>
2023-10-08 01:32:05 +0600

    add to main function

---
+++ 28.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+	// your code goes here
+
+	    int a,b,n;
+	    cin>>n>>a>>b;
+	    cout<<(n-a)<<" "<<(n-a)-b<<endl;
+	    
+	   
+	    
+	    
+	
+	
+	return 0;
+}
